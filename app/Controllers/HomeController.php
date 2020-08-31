0c0008f1ad171cca4b2e8172f74878c86c51b48b
mateuszdabski <mateuszdabski96@gmail.com>
2020-08-31 23:39:41 +0200

    [added] product index

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -13,10 +13,8 @@ class HomeController
     {
         $products = $product -> get();
 
-        //var_dump($products);
-        var_dump($products->first()->title);
-        die();
-
-        return $view->render($response, 'home.twig');
+        return $view->render($response, 'home.twig' , [
+            'products' => $products
+        ]);
     }
 }
