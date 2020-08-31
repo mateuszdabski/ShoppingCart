337b5243c190681d1fe0ab0b859c4ebfb4272df9
mateuszdabski <mateuszdabski96@gmail.com>
2020-08-31 22:59:11 +0200

    [added] database configuration

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,13 +3,20 @@
 namespace Cart\Controllers;
 
 use Slim\Views\Twig;
+use Cart\Models\Product;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class HomeController
 {
-    public function index(Request $request, Response $response, Twig $view)
+    public function index(Request $request, Response $response, Twig $view, Product $product)
     {
+        $products = $product -> get();
+
+        //var_dump($products);
+        var_dump($products->first()->title);
+        die();
+
         return $view->render($response, 'home.twig');
     }
 }
